650ca450dcdaaeb82aab481e48eaba9c37462668
alexislopes <alexisdrops@gmail.com>
2019-05-29 12:19:16 -0300

    created reducers

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+// action creator
+export const selectSong = () => {
+  // Return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song
+  };
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05"
+    },
+    {
+      title: "Macarena",
+      duration: "2:30"
+    },
+    {
+      title: "All Stars",
+      duration: "3:15"
+    },
+    {
+      title: "I Want it That Way",
+      duration: "1:45"
+    }
+  ];
+};
+
+const selectedSongReducer = (selecteSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
